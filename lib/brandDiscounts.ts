e271896a04c5a745188aa35b404ad463894355e2
vanshqamra <VANSH.QAMRA@GMAIL.COM>
2025-08-06 13:16:08 +0530

    Create brandDiscounts.ts

---
+++ lib/brandDiscounts.ts
@@ -0,0 +1,10 @@
+const brandDiscounts: Record<string, number> = {
+  Qualigens: 25,   
+  Borosil: 15,
+  Rankem: 25,
+  Whatman: 20,
+  Avarice: 30,
+  HiMedia: 5,
+}
+
+export default brandDiscounts
