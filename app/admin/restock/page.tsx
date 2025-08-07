d1b26589bfa8d5bbd5f9238fdd98e448e8c09bb7
vanshqamra <VANSH.QAMRA@GMAIL.COM>
2025-08-07 12:18:03 +0530

    Update page.tsx

--- app/admin/restock/page.tsx
+++ app/admin/restock/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+const safeArray = (x: any): any[] => (Array.isArray(x) ? x : []);
 import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
