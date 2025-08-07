c7ebef2857a08b14d19bca31a396901f2a292300
vanshqamra <VANSH.QAMRA@GMAIL.COM>
2025-08-07 19:01:41 +0530

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div className="text-center mt-24">
+      <h1 className="text-3xl font-bold mb-4">Page Not Found</h1>
+      <p className="text-gray-500">The page you are looking for does not exist.</p>
+    </div>
+  );
+}
