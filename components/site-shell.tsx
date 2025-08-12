27ead4adda87c8002446423017a809d324980239
vanshqamra <VANSH.QAMRA@GMAIL.COM>
2025-08-12 12:33:14 +0530

    feat: extend site background

--- components/site-shell.tsx
+++ components/site-shell.tsx
@@ -1,16 +1,14 @@
-"use client"
+"use client";
 
-import { usePathname } from "next/navigation"
-import { Header } from "@/components/header"
-import { Footer } from "@/components/footer"
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export function SiteShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-transparent">
-      {/* header */}
-      {/* if Header has bg-white, keep it (headers are fine), but main must be transparent */}
+      <Header />
       <main className="flex-1 bg-transparent">{children}</main>
-      {/* footer */}
+      <Footer />
     </div>
   );
 }
